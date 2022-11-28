d0245ced26a589028a479675ceb63421e5b16b37
Vitor Augusto <vitoraugusto2010201078@gmail.com>
2022-11-28 16:45:36 -0300

    Midi

--- Midi/js/main.js
+++ Midi/js/main.js
@@ -1,11 +1,30 @@
-function tocaSom( idAudio ) {
+function tocaSom(elementoAudio) {
+  const elemento = document.querySelector(elementoAudio);
 
-  document.querySelector(idAudio).play();
- 
+  if ((elemento != null) & (elemento.localName === "audio")) {
+    elemento.play();
+  } else {
+    alert("Elemento não é um audio");
+  }
 }
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-const listaTeclas = document.querySelectorAll(`.tecla`)
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const intrumento = tecla.classList[1];
 
-for (let i = 0; i < listaTeclas.length; i++) {
-  listaTeclas[i].addEventListener(`click`, tocaSom)
+  const idAudio = `#som_${intrumento}`;
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+
+  tecla.onkeydown = function (e) {
+    console.log(e.code);
+    if (e.keyCode === 13 || e.keyCode === 32) {
+      tecla.classList.add("ativa");
+    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
